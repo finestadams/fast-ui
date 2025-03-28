b6dc46bea6030642a7d53f20cdc75f27d4f44f5a
finestadams <subaru4teb@gmail.com>
2025-03-28 02:39:28 +0100

    fix: input ui

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -3,32 +3,46 @@ import clsx from "clsx";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  label?: string;
   error?: string;
   icon?: React.ReactNode;
+  inputSize?: "sm" | "md" | "lg";
 }
 
 export const Input: React.FC<InputProps> = ({
-  label,
   error,
   icon,
+  inputSize = "md",
   className,
+  type = "text", // Default type is "text"
   ...props
 }) => {
   return (
     <div className="flex flex-col space-y-1">
-      {label && (
-        <label className="text-sm font-medium text-gray-700">{label}</label>
-      )}
       <div
         className={clsx(
-          "flex items-center border rounded-md px-3 py-2 focus-within:ring-2 focus-within:ring-blue-500 transition",
-          error ? "border-red-500" : "border-gray-300",
+          "flex items-center border rounded-md border-gray-500 transition focus-within:ring-2",
+          {
+            "px-2 py-1 text-xs": inputSize === "sm",
+            "px-3 py-2 text-sm": inputSize === "md",
+            "px-4 py-3 text-base": inputSize === "lg",
+          },
+          {
+            "focus-within:ring-primary": !error,
+            "border-red-500 focus-within:ring-red-500": error,
+          },
           className
         )}
       >
-        {icon && <span className="mr-2 text-gray-500">{icon}</span>}
-        <input className="w-full outline-none bg-transparent" {...props} />
+        {icon && <span className="mr-2  ">{icon}</span>}
+        <input
+          // Pass the type prop to the input element
+          className={clsx(
+            "w-full outline-none bg-transparent",
+            error ? "text-red-500" : "text-black"
+          )}
+          aria-invalid={!!error}
+          {...props}
+        />
       </div>
       {error && <p className="text-xs text-red-500">{error}</p>}
     </div>
